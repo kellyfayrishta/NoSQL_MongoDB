7d295f65bbb739a23c7557f36d6ee26940bcdff8
Kelly Williams <105178236+kellyfayrishta@users.noreply.github.com>
2023-02-12 14:18:08 -0500

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const apiRoutes = require("./api");
+const userRoutes = require("./user-routes");
+const thoughtRoutes = require("./thought-routes");
 
-router.use("/api", apiRoutes);
-
-router.use((req, res) => res.send("Wrong route!"));
+router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
 
 module.exports = router;
