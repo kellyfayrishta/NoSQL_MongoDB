edeb8fbdc197c3c4f0db58b1c527d5bb6a85b25e
Kelly Williams <105178236+kellyfayrishta@users.noreply.github.com>
2023-02-12 15:14:26 -0500

    fixed file structure

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const userRoutes = require("./user-routes");
-const thoughtRoutes = require("./thought-routes");
+const userRoutes = require("./api/user-routes");
+const thoughtRoutes = require("./api/thought-routes");
 
-router.use("/users", userRoutes);
-router.use("/thoughts", thoughtRoutes);
+router.use("/users", user-routes);
+router.use("/thoughts", thought-routes);
 
 module.exports = router;
