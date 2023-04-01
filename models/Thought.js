09c283fa2cca30d3d9d254eb77e56cff4a9fe973
Kelly Williams <105178236+kellyfayrishta@users.noreply.github.com>
2023-04-01 14:20:49 -0400

    fixed error in thought.js file

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-const { Schema, model } = require("mongoose");
 const reactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
 
